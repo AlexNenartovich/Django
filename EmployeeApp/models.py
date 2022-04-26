081a7f3f18cc78da728ceef76d95ecca92d333e4
AlexNenartovich <aliaksandrhn@gmail.com>
2022-04-26 15:43:56 -0700

    First commit

---
+++ EmployeeApp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Employees(models.Model):
+    EmployeeId = models.AutoField(primary_key=True)
+    EmployeeName = models.CharField(max_length=500)
+    Department = models.CharField(max_length=500)
+   
---
+++ EmployeeApp/urls.py
@@ -0,0 +1,9 @@
+##from django.conf.urls import url
+from django.urls import path
+from EmployeeApp import views
+
+urlpatterns = [
+    path(r'hello/', views.employeeApi),
+
+    path('hello/<int:id>/', views.employeeApi)
+]
